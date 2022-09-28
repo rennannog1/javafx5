412f264f74e8faacdfa5e63d63de9742b87b581c
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-27 21:28:51 -0300

    button test

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-
+	
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,15 +8,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+	@Override
 	public void start(Stage stage) {
 		try {
 			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-			Scene scene = new Scene(parent);
-			stage.setScene(scene);
-			stage.show();
-		} catch (IOException e) {
+	    		Scene scene = new Scene(parent);
+	        	stage.setScene(scene);
+	        	stage.show();
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
